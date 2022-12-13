5b07c85c18f1ef05143f666a669a191ff4a1eeaf
dawit2123 <dawitmunie111@gmail.com>
2022-12-13 16:59:46 +0300

    Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,41 @@
 #include<stdio.h>
 
 /**
- * main - prints the first 50 Fibonacci numbers.
+ * main - first 50 fibonatchi.
  *
  * Return: void.
  */
 int main(void)
 {
-	unsigned long int num1, num10, num11, number2;
+	unsigned long int n, n0, n1, l, l0, l1, m, m0, m1, k, y;
 
-    unsigned long int number20, number21, e, e0, e1, y, num3;
+	n = 1;
+	l = 2;
+	m = n + l;
 
-	num1 = 1;
-	number2 = 2;
-	e = num1 + number2;
-
-	printf("%lu, ", num1);
-	printf("%lu, ", number2);
+	printf("%lu, ", n);
+	printf("%lu, ", l);
 	for (y = 3; y < 89; y++)
 	{
-		printf("%lu, ", e);
-		num1 = number2;
-		number2 = e;
-		e = num1 + number2;
+		printf("%lu, ", m);
+		n = l;
+		l = m;
+		m = i + l;
 	}
-	number20 = number2 / 1000000000;
-	number21 = number2 % 1000000000;
-	e0 = e / 1000000000;
-	e1 = e % 1000000000;
-	for (num3 = 89; num3 < 98; num3++)
+	l0 = l / 1000000000;
+	l1 = l % 1000000000;
+	m0 = m / 1000000000;
+	m1 = m % 1000000000;
+	for (k = 89; k < 98; k++)
 	{
-		printf("%lu%lu, ", e0, e1);
-		num10 = number20;
-		num11 = number21;
-		number20 = e0;
-		number21 = e1;
-		e0 = num10 + number20 + ((num11 + number21) / 1000000000);
-		e1 = (num11 + number21) % 1000000000;
+		printf("%lu%lu, ", m0, m1);
+		n0 = l0;
+		n1 = l1;
+		l0 = m0;
+		l1 = m1;
+		m0 = n0 + l0 + ((n1 + l1) / 1000000000);
+		m1 = (n1 + l1) % 1000000000;
 	}
-printf("%lu%lu\n", e0, e1);
+printf("%lu%lu\n", m0, m1);
 	return (0);
 }
