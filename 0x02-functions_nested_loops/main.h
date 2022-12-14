efe9561c3ca1a65a72faf0862ae1c77f67725d01
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2022-12-14 13:14:46 +0300

    Done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: main.h
- * Auth: Gezish
+ * Auth: Dawit
  * Desc: Header file containing declarations for all functions
  *       used in the 0x02-functions_nested_loops directory.
  */
