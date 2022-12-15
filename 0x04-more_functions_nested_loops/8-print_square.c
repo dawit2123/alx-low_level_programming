f114476d13c84d4065d3a336963a27f746257f3e
dawit2123 <dawitmunie111@gmail.com>
2022-12-15 14:26:58 +0300

    prints a square, followed by a new line.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - prints a square, followed by a new line;
- * @size: size of the square
+ * @size: size of the square.
  */
 void print_square(int size)
 {
@@ -13,9 +13,9 @@ void print_square(int size)
 	{
 		int num1, num2;
 
-		for (num1 = 0; num1 < snum1ze; num1++)
+		for (num1 = 0; num1 < size; num1++)
 		{
-			for (num2 = 0; num2 < snum1ze; num2++)
+			for (num2 = 0; num2 < size; num2++)
 			{
 				_putchar('#');
 			}
