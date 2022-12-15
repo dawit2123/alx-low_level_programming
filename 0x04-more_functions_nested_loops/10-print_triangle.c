b2fb922272ab6010004dc11f3403d019b5a30a4d
dawit2123 <dawitmunie111@gmail.com>
2022-12-15 14:27:28 +0300

    prints a triangle, followed by a new line.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,9 +13,9 @@ void print_triangle(int size)
 	{
 		int n1, n2;
 
-		for (n1 = 1; n1 <= sn1ze; n1++)
+		for (n1 = 1; n1 <= size; n1++)
 		{
-			for (n2 = n1; n2 < sn1ze; n2++)
+			for (n2 = n1; n2 < size; n2++)
 			{
 				_putchar(' ');
 			}
