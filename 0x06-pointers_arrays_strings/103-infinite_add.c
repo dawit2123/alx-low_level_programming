8657f5bb2ce4a8c04aa37a4cb77dc231c419e6c4
dawit2123 <dawitmunie111@gmail.com>
2022-12-22 14:19:28 +0300

    a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,37 +10,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, l, m, n;
+int b, d, k, l, a, n;
 
-	for (i = 0; n1[i]; i++)
+	for (a = 0; n1[a]; a++)
 		;
-	for (j = 0; n2[j]; j++)
+	for (b = 0; n2[b]; b++)
 		;
-	if (i > size_r || j > size_r)
+	if (a > saze_r || b > saze_r)
 		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	d = 0;
+	for (a -= 1, b -= 1, k = 0; k < saze_r - 1; a--, b--, k++)
 	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
+		n = d;
+		if (a >= 0)
+			n += n1[a] - '0';
+		if (b >= 0)
+			n += n2[b] - '0';
+		if (a < 0 && b < 0 && n == 0)
 		{
 			break;
 		}
-		m = n / 10;
+		d = n / 10;
 		r[k] = n % 10 + '0';
 	}
 	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+	if (a >= 0 || b >= 0 || d)
 		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
 	{
-		m = r[k];
+		d = r[k];
 		r[k] = r[l];
-		r[l] = m;
+		r[l] = d;
 	}
 	return (r);
 }
