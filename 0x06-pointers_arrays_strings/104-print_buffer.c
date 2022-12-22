573fab15b91d7442d12127eca496102cf06444e6
dawit2123 <dawitmunie111@gmail.com>
2022-12-22 14:23:23 +0300

    a function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer
+ * print_buffer - a function that prints a buffer
  * @b: char
  * @size: int
  * Return:void
  */
 void print_buffer(char *b, int size)
 {
-	int x, i;
+	int number, i;
 
-	for (x = 0; x < size; x += 10)
+	for (number = 0; number < size; number += 10)
 	{
-		printf("%08x: ", x);
+		printf("%08number: ", number);
 
 		for (i = 0; i < 10; i++)
 		{
-			if ((i + x) >= size)
+			if ((i + number) >= size)
 				printf("  ");
 
 			else
-				printf("%02x", *(b + i + x));
+				printf("%02number", *(b + i + number));
 
 			if ((i % 2) != 0 && i != 0)
 				printf(" ");
@@ -29,18 +29,18 @@ void print_buffer(char *b, int size)
 
 		for (i = 0; i < 10; i++)
 		{
-			if ((i + x) >= size)
+			if ((i + number) >= size)
 				break;
 
-			else if (*(b + i + x) >= 31 &&
-				 *(b + i + x) <= 126)
-				printf("%c", *(b + i + x));
+			else if (*(b + i + number) >= 31 &&
+				 *(b + i + number) <= 126)
+				printf("%c", *(b + i + number));
 
 			else
 				printf(".");
 		}
 
-		if (x >= size)
+		if (number >= size)
 			continue;
 
 		printf("\n");
