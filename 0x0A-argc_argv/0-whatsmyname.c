879852653d2795fca8e83946e704274fe5769d70
dawit2123 <dawitmunie111@gmail.com>
2023-01-06 21:29:46 +0300

    a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
  * main - A function that Prints the program name, followed by a new line.
  * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * @argv: An array of pointers to the arguments
  *
  * Return: Always o.
  */
