3945d985b2089c0f99432a7bbc8034925163cc42
dawit2123 <dawitmunie111@gmail.com>
2023-01-12 12:50:55 +0300

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * _is_zero - determines if any number is zero
+ * _is_zero - a program that multiplies two positive numbers.
  * @argv: argument vector.
  *
  * Return: no return.
@@ -11,23 +11,23 @@
 
 void _is_zero(char *argv[])
 {
-	int i, isn1 = 1, isn2 = 1;
+	int i, number1 = 1, number2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
 		if (argv[1][i] != '0')
 		{
-			isn1 = 0;
+			number1 = 0;
 			break;
 		}
 
 	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
-			isn2 = 0;
+			number2 = 0;
 			break;
 		}
 
-	if (isn1 == 1 || isn2 == 1)
+	if (number1 == 1 || number2 == 1)
 	{
 		printf("0\n");
 		exit(0);
@@ -63,21 +63,21 @@ char *_initialize_array(char *ar, int lar)
 
 int _checknum(char *argv[], int n)
 {
-	int ln;
+	int num3;
 
-	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
+	for (num3 = 0; argv[n][num3]; num3++)
+		if (!isdigit(argv[n][num3]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
 
-	return (ln);
+	return (num3);
 }
 
 /**
  * main - Entry point.
- * program that multiplies two positive numbers.
+ * a program that multiplies two positive numbers.
  * @argc: number of arguments.
  * @argv: arguments vector.
  *
@@ -85,44 +85,44 @@ int _checknum(char *argv[], int n)
  */
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, lnout, add, addl, i, j, k, ca;
-	char *nout;
+	int num1, num2, lnout, sum, suml, i, j, k, caus;
+	char *ch;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
-	if (nout == NULL)
+	num1 = _checknum(argv, 1), num2 = _checknum(argv, 2);
+	_is_zero(argv), lch = num1 + num2, ch = malloc(lch + 1);
+	if (ch == NULL)
 		printf("Error\n"), exit(98);
-	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	ch = _initialize_array(ch, lch);
+	k = lch - 1, i = num1 - 1, j = num2 - 1, caus = suml = 0;
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
 		{
-			if (addl > 0)
+			if (suml > 0)
 			{
-				add = (nout[k] - '0') + addl;
-				if (add > 9)
-					nout[k - 1] = (add / 10) + '0';
-				nout[k] = (add % 10) + '0';
+				sum = (ch[k] - '0') + suml;
+				if (sum > 9)
+					ch[k - 1] = (sum / 10) + '0';
+				ch[k] = (sum % 10) + '0';
 			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+			i = num1 - 1, j--, suml = 0, caus++, k = lch - (1 + caus);
 		}
 		if (j < 0)
 		{
-			if (nout[0] != '0')
+			if (ch[0] != '0')
 				break;
-			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+			lch--;
+			free(ch), ch = malloc(lch + 1), ch = _initialize_array(ch, lch);
+			k = lch - 1, i = num1 - 1, j = num2 - 1, caus = suml = 0;
 		}
 		if (j >= 0)
 		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
+			sum = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (ch[k] - '0') + suml;
+			suml = sum / 10, ch[k] = (sum % 10) + '0';
 		}
 	}
-	printf("%s\n", nout);
+	printf("%s\n", ch);
 	return (0);
 }
