630075a087097f88247fef4c5396866d67d719ec
dawit2123 <dawitmunie111@gmail.com>
2023-01-18 19:57:50 +0300

    a function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - a function that prints a name
+ * print_name - a function that prints a name.
  * @name: name to print
  * @f: function to print with
  *
