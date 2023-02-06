7e0164230f2e83863ccba50b9c99653214bbb759
dawit2123 <dawitzewdu2123@gmail.com>
2023-02-06 21:14:54 +0300

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,23 +7,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, size;
+	int num1, capacity;
 	/* task condition */
 	if (!filename)
 		return (-1);
 	/* rw------- permission if it exist truncate it*/
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	num1 = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (num1 == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		/* for write we need the size */
-		for (size = 0; text_content[size] != '\0'; size++)
+		/* for write we need the capacity */
+		for (capacity = 0; text_content[capacity] != '\0'; capacity++)
 			;
-		/*  ssize_t write(int fd, const void *buf, size_t count); */
-		write(fd, text_content, size);
+		/*  scapacity_t write(int num1, const void *buf, capacity_t count); */
+		write(num1, text_content, capacity);
 	}
-	close(fd);
+	close(num1);
 	return (1);
 }
